b04c6111cf30b7a64aa75688bce9f7af6f9bdd10
SHOIYAN <shoiyan9@gmail.com>
2025-09-08 00:35:03 +0300

    refactor: rename square.forEach to squares.forEach for clarity

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ for (let i = 0; i < 256; i++){
     container.appendChild(square);
 }
 // hover squares
-const square = document.querySelectorAll('.square');
-square.forEach((square)=>{
+const squares = document.querySelectorAll('.square');
+squares.forEach((square)=>{
     square.addEventListener("mouseover", () => {
         square.classList.add('hover-state');
     });
